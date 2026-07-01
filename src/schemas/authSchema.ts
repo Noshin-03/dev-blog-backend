24b8d86c320ac1022d7b841a19c99a2841f73263
Noshin-03 <bsse1503@iit.du.ac.bd>
2026-07-01 10:38:23 +0600

    feat: integrate Zod validation

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -25,16 +25,16 @@ export const changePasswordSchema = z.object({
         oldPassword: z.string().min(1, 'Old password is required'),
         newPassword: z
             .string()
-            .min(8, 'New password must be at least 8 characters')
+            .min(8, 'Password must be at least 8 characters')
             .regex(
                 /[A-Z]/,
-                'New password must contain at least one uppercase letter',
+                'Password must contain at least one uppercase letter',
             )
             .regex(
                 /[a-z]/,
-                'New password must contain at least one lowercase letter',
+                'Password must contain at least one lowercase letter',
             )
-            .regex(/[0-9]/, 'New password must contain at least one number'),
+            .regex(/[0-9]/, 'Password must contain at least one number'),
     }),
 });
 
--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,4 +1,4 @@
-import { email, z } from 'zod';
+import { z } from 'zod';
 
 export const createUserSchema = z.object({
     body: z.object({
@@ -6,23 +6,34 @@ export const createUserSchema = z.object({
             .string()
             .min(3, 'Username must be at least 3 characters')
             .max(30, 'Username must be at most 30 characters'),
-        name: z.string().min(3, 'Name is required'),
-        email: z.string().email(),
-        role: z.enum(['ADMIN', 'USER']).optional(),
+        name: z.string().min(3, 'Name must be at least 3 characters'),
+        email: z.email(),
+        role: z.optional(z.enum(['ADMIN', 'USER'])),
     }),
 });
 
+//for POST, PATCH
 export const updateUserSchema = z.object({
     body: z
         .object({
-            username: z.string().min(3).max(30).optional,
-            name: z.string().min(3).optional(),
-            email: z.string().email().optional(),
-            role: z.enum(['ADMIN', 'USER']).optional(),
+            username: z.optional(z.string().min(3).max(30)),
+            name: z.optional(z.string().min(1)),
+            email: z.optional(z.email()),
+            role: z.optional(z.enum(['ADMIN', 'USER'])),
         })
         .refine((data) => Object.keys(data).length > 0, {
             message: 'At least one field must be provided to update',
         }),
+    params: z.object({
+        userId: z.string().regex(/^\d+$/, 'userId must be a number'),
+    }),
+});
+
+//for GET, DELETE
+export const userIdParamSchema = z.object({
+    params: z.object({
+        userId: z.string().regex(/^\d+$/, 'userId must be a number'),
+    }),
 });
 
 export type CreateUserInput = z.infer<typeof createUserSchema>['body'];
