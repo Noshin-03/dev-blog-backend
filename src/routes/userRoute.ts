bf1575e0737617732507ae4d402cfed86fb06a10
Noshin-03 <bsse1503@iit.du.ac.bd>
2026-07-09 09:11:11 +0600

    refactor: store validated request data in middleware

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -23,7 +23,7 @@ router.get(
 );
 router.get(
     '/:userId',
-    validate(idSchema),
+    validate(userIdParamSchema),
     asyncHandler(UserController.getUserById),
 );
 router.patch(
@@ -33,7 +33,7 @@ router.patch(
 );
 router.delete(
     '/:userId',
-    validate(idSchema),
+    validate(userIdParamSchema),
     asyncHandler(UserController.deleteUser),
 );
 
